41a97cc4ad1920bdf0f7dfd6be01b64f1cba451c
Blizzart98 <castrio98@gmail.com>
2019-09-24 18:15:08 +0200

    Prima classe, mancano metodi

---
+++ ProgettoOOP/src/AidMainObjective.java
@@ -0,0 +1,12 @@
+
+public class AidMainObjective {
+	private String freq;
+	private String geo;
+	private String unit;
+	private String Objective;
+	private float[] aids;
+	
+	AidMainObjective(){
+
+}
+}
