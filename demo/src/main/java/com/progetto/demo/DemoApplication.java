ee946362b0989b3e19260b17dffb2783dfad6553
Blizzart98 <castrio98@gmail.com>
2019-09-29 18:14:29 +0200

    Prova Parsing

--- demo/src/main/java/com/progetto/demo/DemoApplication.java
+++ demo/src/main/java/com/progetto/demo/DemoApplication.java
@@ -1,9 +1,10 @@
 package com.progetto.demo;
+import com.progetto.demo.model.*;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.util.Vector;
 
- 
 //Classe da cui parte l'esecuzione del programma, avviando il servizio
 
 import org.springframework.boot.SpringApplication;
@@ -14,10 +15,14 @@ import com.progetto.demo.url.Parsing;
 
 @SpringBootApplication
 public class DemoApplication {
+	private static Vector<Aid> csv;
+	
 
 	public static void main(String[] args) throws IOException, ParseException, org.json.simple.parser.ParseException {
 		SpringApplication.run(DemoApplication.class, args);
 		Download.selectUrl();
+		csv=Parsing.fileParsing();
+		
 	}
 
 }
--- demo/src/main/java/com/progetto/demo/url/Parsing.java
+++ demo/src/main/java/com/progetto/demo/url/Parsing.java
@@ -1,10 +1,53 @@
 package com.progetto.demo.url;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Scanner;
+import java.util.Vector;
+
+import com.progetto.demo.model.*;
 
 public class Parsing {
+	final static String DELIMITER = ",;|"; //delimitatori che andremo a cercare nel CSV
+	private static String[] aux; //variabile di supporto per i campi
 	
+	/*
+	 * Funzione che effettua il parsing del CSV e restituisce una tabella strutturata
+	 */
+	
+	public static Vector<Aid> fileParsing() throws FileNotFoundException, IOException{
+		
+		Vector<Aid> tab= new Vector<Aid>(); //la tabella sarà un vettore di oggetti di classe Aid
+		
+		try(BufferedReader br = new BufferedReader(new FileReader("file/data.csv"))){
+			String line;
+			int y=0;
+			while((line = br.readLine()) != null) {
+				String[] campi = line.split(DELIMITER);
+				if(y!=0) //condizione necessaria per riconoscere quali sono  nostri campi, y sarà 0 solo la prima volta, poi memorizzerà l'anno iniziale
+					tab.add(new Aid(campi[0].charAt(0), campi[1],campi[2], campi[3], addYA(campi,y) ));
+				else {
+					aux = campi;
+					campi[4]=campi[4].trim(); //levo gli spazi
+					y=Integer.parseInt(campi[4]);
+				}
+		}
+			br.close();
+		}catch(IOException i) {
+			i.printStackTrace();
+		}
+		return tab;
+	}
+	
+	private static Vector<YA> addYA(String[] campi,int year) {
+		Vector<YA> aidLine = new Vector<YA>();
+		for(int i=4;i<campi.length;i++) {
+		aidLine.add(new YA((year++), Double.parseDouble(campi[i].trim())));
+		}
+		return aidLine;
+	}
 }
+
+
+
