967e34acf3a81af031a4a5426e1a49900257dca8
Lorenzo <Lorenzo@DESKTOP-VD76MST>
2019-10-02 21:09:51 +0200

    Aggiunti primi filtri

--- demo/src/main/java/com/progetto/demo/controller/simpleRestController.java
+++ demo/src/main/java/com/progetto/demo/controller/simpleRestController.java
@@ -6,6 +6,7 @@ import java.util.Vector;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.progetto.demo.model.Aid;
@@ -16,31 +17,29 @@ import com.progetto.demo.url.Parsing;
 
 public class simpleRestController {
 	
-	public static Vector<Aid> tab;
-	
-	/*	@GetMapping("/aid")
-	 * public Aid getObj() throws FileNotException, IOException {
-	return new Aid();
-	}*/
+	private static Vector<Aid> tab;
+	private static Vector<Aid> filtered;
 	
 	@RequestMapping("/aid")
 	public String controls()
 	{
 		return "Richieste disponibili: -/get: riceve i dati degli Aid in formato JSon";
 	}
+	
 	@GetMapping("/aid/get")
-	public Vector<Aid> getAll()//Stampa l'intera collezione di oggetti aid convertita dal file alla richiesta "-/get"
+	public Vector<Aid> getFiltered(@RequestParam(value="filter",defaultValue="", required=false) String filter) throws FileNotFoundException, IOException
 	{
-		try {
-			tab=Parsing.fileParsing();
-			
-		} catch (FileNotFoundException e) {
-			
-			e.printStackTrace();
-		} catch (IOException e) {
-			
-			e.printStackTrace();
+		tab=Parsing.fileParsing();
+		if(filter.equals(""))
+			{
+				return tab;
+			}
+		else
+		{
+			String[] attributes=filter.split("=");
+			filtered=filterUtils.filterAttributes(attributes[0],attributes[1],tab);
+			return filtered;
 		}
-		return tab;
+		
 	}
 }
